0cced7737f96c4ec1b16538bcefba8a59e190e9b
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-10-20 23:53:35 +0300

    Searchbar and filtering UI

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -13,8 +13,8 @@ function Button({
                 "btn flex flex-row justify-center items-center px-[7px] py-[10px] gap-[10px] w-[230px] h-[48px] border border-[#0D0C22] rounded-full",
                 bgColor,
                 textColor,
-                hoverBgColor,  // Hover background color
-                hoverTextColor, // Hover text color
+                hoverBgColor,
+                hoverTextColor,
                 "hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out"
             )}
         >

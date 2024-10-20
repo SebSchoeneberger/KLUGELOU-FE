7c2a201fd7e850e1c1429133c470f3651f4b0f77
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-10-20 19:35:36 +0300

    Searchbar Section

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+    return ( 
+        <>
+            <label className="input flex items-center gap-2 box-border w-full max-w-[300px] h-[48px] bg-white border border-black rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                </svg>
+                <input type="text" className="grow" placeholder="Email Address" />
+            </label>
+        </>
+     );
+}
+
+export default SearchBar;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Button from "../Components/Button";
 import Carousel from "../Components/Carousel";
+import SearchBar from "../Components/SearchBar";
 
 function Home() {
     return ( 
@@ -14,8 +15,8 @@ function Home() {
                     </p>
                     <div className="flex flex-col md:flex-row justify-center items-center gap-[16px] md:gap-[24px]">
                         <label className="input flex items-center gap-2 box-border w-full max-w-[300px] h-[48px] bg-white border border-black rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                             </svg>
                             <input type="text" className="grow" placeholder="Email Address" />
                         </label>
@@ -26,6 +27,16 @@ function Home() {
 
             <section>
                 <Carousel />
+            </section>
+
+            <section>
+
+                <h1 className="font-nunito text-4xl font-medium text-customBlack text-center pt-24 pb-20">
+                    Explore the amazing world of gemstones
+                </h1>
+
+                <SearchBar />
+
             </section>
         </>
     );
